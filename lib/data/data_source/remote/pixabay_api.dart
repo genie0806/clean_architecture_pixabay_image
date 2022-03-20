6b1b904f25e025ccf7a83a53f4c38cd93cd856b3
genie0806 <genie0806@gmail.com>
2022-03-20 23:14:25 +0900

    searchKeyword 설명

--- lib/data/data_source/remote/pixabay_api.dart
+++ lib/data/data_source/remote/pixabay_api.dart
@@ -10,9 +10,10 @@ class PixaBayApi {
     try {
       final response = await http.get(Uri.parse(
           '$baseUrl?key=17828481-17c071c7f8eadf406822fada3&q=$searchKeyword&image_type=photo'));
+      // searchKeyword를 외부에서 받아서 URL을 변경한다.
       Map<String, dynamic> jsonResponse = convert.jsonDecode(response.body);
       SearchModel searchData = SearchModel.fromJson(jsonResponse);
-    
+
       return Result.success(searchData);
     } on Exception catch (e) {
       return Result.error(e);
